10b8de7d5f8c58db1dc2721753b89f878e590090
Maja Magel <mmagel@ukaachen.de>
2022-03-21 15:07:38 +0100

    Initial commit

---
+++ count_lines.sh
@@ -0,0 +1,10 @@
+#Small script to count lines in tsv files
+#Maja Magel <mmagel@ukaachen.de>
+#2022-03-21
+#copied from Konrad Förstner
+#Datei-Endung .sh weist auf shell-lesson hin
+
+echo "Counting lines in all tsv files"
+wc -l *tsv
+echo "Done! Have a nice day"
+
